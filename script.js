090c60437fa6cad053875d111e0e52be4280be23
neeraj <neerajkumarghildiyal@gmail.com>
2023-08-15 04:40:11 +0530

    We have Implemented localstorage to toggle between dark and light mode

--- script.js
+++ script.js
@@ -96,11 +96,13 @@ function switchTheme(event){
 
     if(event.target.checked){
 
-        document.documentElement.setAttribute('data-theme', 'dark')
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     }else
     {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode();
 
     }
@@ -110,5 +112,29 @@ function switchTheme(event){
 
 // Event Listener 
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check LocalStorage For Theme
+
+const currentTheme = localStorage.getItem('theme');
+// console.log(currentTheme); 
+
+if(currentTheme){
+
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if(currentTheme === 'dark'){
+
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+
+}
+
+
+
+
+
+
+
 
 
